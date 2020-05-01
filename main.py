209ea4c5c905ffc43f899c452a61681211e6a5d8
Rafael Dias <rafaelprazeresdias@gmail.com>
2020-05-01 04:04:15 +0100

    Main Menu Completed

--- main.py
+++ main.py
@@ -5,19 +5,36 @@ max = 20
 
 random = random.randint(min, max)
 
+settings = False
+
 appIsRunning = True
 gameIsRunning = False
+Computer = False
 
 print("Hello!")
 print("Welcome to \"Guess The Number\", hope you have a wonderful day.\n")
 
 while appIsRunning == True:
-    print("[1] Guess the Number\n[2] Computer  Guesses the Number\n[3] Settings\n[0] Exit")
-    option = int(input("Option: "))
+    if settings == False:
+        print("[1] Guess the Number\n[2] Computer  Guesses the Number\n[3] Settings\n[0] Exit")
+        option = int(input("Option: "))
+    else:
+        #SETTINGS
+        print("settings")
 
     if option >= 0:
         if option <= 3:
-            appIsRunning = False
+            if option == 0:
+                appIsRunning = False
+                gameisRunning = False
+            elif option == 1:
+                appIsRunning = False
+                gameisRunning = True
+            elif option == 2:
+                appIsRunning = False
+                gameisRunning = True
+            elif option == 3:
+                settings = True
     else:
         print("\nERROR: Option not valid.")
 
